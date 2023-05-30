46c9f1eed7e9415644619bdf7e230eecfb438d18
Omikron-mnw <minomushi.ff15.07.08@gmail.com>
2023-05-30 23:17:37 +0900

    xdebug追加

--- Makefile
+++ Makefile
@@ -1,14 +1,20 @@
-# NAME=
-# VERSION=
+.GOAL_DEFULT = help
 
-build:
+help:
+.PHONY: help
+
+up:
 	docker-compose up --build -d
+.PHONY: up
 
 app_in:
 	docker-compose exec poll_app bash
+.PHONY: app_in
 
 db-in:
 	docker-compose exec web_dev_app bash
+.PHONY: db_in
 
 sql:
 	mysql -u root -p web_dev_db
+.PHONY: sql
--- app/config.php
+++ app/config.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 define('CURRENT_URI', $_SERVER['REQUEST_URI']);
-if(preg_match("/(.+(start|end))/i", CURRENT_URI, $match)) {
-    define('BASE_CONTEXT_PATH', $match[0] . '/');
-}
+// if(preg_match("/(.+(start|end))/i", CURRENT_URI, $match)) {
+    // define('BASE_CONTEXT_PATH', $match[0] . '/');
+    define('BASE_CONTEXT_PATH', '/');
+// }
 
 define('BASE_IMAGE_PATH', BASE_CONTEXT_PATH . 'images/');
 define('BASE_JS_PATH', BASE_CONTEXT_PATH . 'js/');
