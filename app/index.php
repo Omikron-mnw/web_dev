a93e17f7e21ef77934822ed588bdc8504fc21eea
Omikron-mnw <minomushi.ff15.07.08@gmail.com>
2023-06-08 22:43:17 +0900

    feature Makefileのhelp追加

--- app/index.php
+++ app/index.php
@@ -47,14 +47,14 @@ try {
     $url = parse_url(CURRENT_URI);
     $rpath = str_replace(BASE_CONTEXT_PATH, '', $url['path']);
     $method = strtolower($_SERVER['REQUEST_METHOD']);
-    
+
     route($rpath, $method);
-    
+
     \partials\footer();
-    
+
 } catch(Throwable $e) {
 
     die('<h1>何かが凄くおかしいようです。</h1>');
-    
+
 }
 
